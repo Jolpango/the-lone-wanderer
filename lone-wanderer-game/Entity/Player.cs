4931c1d8b86f63294fa778b3920094fb6139276c
Medo Osman <medoosman@outlook.com>
2023-12-09 13:02:46 +0100

    Added Player class & a bunch of new sprites

---
+++ lone-wanderer-game/Entity/Player.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Input;
+using MonoGame.Extended.Input.InputListeners;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+
+namespace LoneWandererGame.Entity
+{
+    class Player
+    {
+        private Game1 game;
+
+        private Vector2 position = new Vector2(0, 0);
+        private float rotation = 0;
+        private Vector2 scale = new Vector2(1, 1);
+        private AnimatedSprite sprite;
+
+        public Player(Game1 game) { this.game = game; }
+
+        public void LoadContent()
+        {
+            var spriteSheet = game.Content.Load<SpriteSheet>("Sprites/player_animations.sf", new JsonContentLoader());
+            sprite = new AnimatedSprite(spriteSheet);
+
+            sprite.Play("idle_up_down");
+            position = new Vector2(100, 100);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            var dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float walkSpeed = dt * 128;
+            var keyboardState = KeyboardExtended.GetState();
+            var animation = "idle_up_down";
+            SpriteEffects flip = SpriteEffects.None;
+            Color colorTint = new Color(1, 1, 1, 1);
+
+            if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
+            {
+                animation = "run_up_down";
+                position.Y -= walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
+            {
+                animation = "run_up_down";
+                position.Y += walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+            {
+                animation = "run_left_right";
+                position.X -= walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
+            {
+                animation = "run_left_right";
+                position.X += walkSpeed;
+                flip = SpriteEffects.FlipHorizontally;
+            }
+
+            sprite.Effect = flip;
+            sprite.Play(animation);
+            sprite.Update(dt);
+        }
+
+        public void Draw()
+        {
+            game.SpriteBatch.Draw(sprite, position);
+        }
+    }
+}
--- lone-wanderer-game/GameScreens/PlayScreen.cs
+++ lone-wanderer-game/GameScreens/PlayScreen.cs
@@ -5,14 +5,15 @@ using MonoGame.Extended.Screens;
 using MonoGame.Extended.Sprites;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended.Input;
+using LoneWandererGame.Entity;
 
 namespace LoneWandererGame.GameScreens
 {
     public class PlayScreen : GameScreen
     {
         private new Game1 Game => (Game1)base.Game;
-        private Vector2 guyPosition = new Vector2(0, 0);
-        private Texture2D guySprite;
+
+        private Player player;
 
         public PlayScreen(Game1 game): base(game) { }
 
@@ -20,7 +21,8 @@ namespace LoneWandererGame.GameScreens
         {
             base.LoadContent();
             Game.KeyboardListener.KeyPressed += menuactions;
-            guySprite = Game.Content.Load<Texture2D>("Sprites/guy");
+            player = new Player(Game);
+            player.LoadContent();
         }
 
         private void menuactions(object sender, KeyboardEventArgs e)
@@ -34,28 +36,14 @@ namespace LoneWandererGame.GameScreens
         public override void UnloadContent()
         {
             base.UnloadContent();
-            Game.KeyboardListener.KeyTyped -= menuactions;
+            Game.KeyboardListener.KeyPressed -= menuactions;
         }
 
         public override void Update(GameTime gameTime)
         {
             var keyboardState = KeyboardExtended.GetState();
-            if (keyboardState.IsKeyDown(Keys.W))
-            {
-                guyPosition.Y--;
-            }
-            if (keyboardState.IsKeyDown(Keys.S))
-            {
-                guyPosition.Y++;
-            }
-            if (keyboardState.IsKeyDown(Keys.A))
-            {
-                guyPosition.X--;
-            }
-            if (keyboardState.IsKeyDown(Keys.D))
-            {
-                guyPosition.X++;
-            }
+
+            player.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
@@ -63,7 +51,7 @@ namespace LoneWandererGame.GameScreens
             Game.GraphicsDevice.Clear(Color.Green);
             Game.SpriteBatch.Begin();
             Game.SpriteBatch.DrawString(Game.RegularFont, "Play Screen", Vector2.Zero, Color.White);
-            Game.SpriteBatch.Draw(guySprite, guyPosition, null, Color.White);
+            player.Draw();
             Game.SpriteBatch.End();
         }
     }
