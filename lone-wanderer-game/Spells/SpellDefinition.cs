1aa243c348689f73347833fd4c8c9a4249ce314f
Joel Funk Persson <joel.funkpersson@gmail.com>
2023-12-09 14:39:40 +0100

    Spell definitions

---
+++ lone-wanderer-game/Spells/SpellDefinition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoneWandererGame.Spells
+{
+    public class SpellDefinition
+    {
+        public Type SpellType { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Asset { get; set; }
+        public int CurrentLevel { get; set; } = 0;
+        public float Timer { get; set; }
+        public float Speed { get; set; }
+        public List<SpellLevelDefinition> LevelDefinitions { get; set; }
+    }
+}
