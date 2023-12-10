898052458df27757f4dbff1bce67a10dd0a654e3
Medo Osman <medoosman@outlook.com>
2023-12-10 20:05:01 +0100

    Rocks now have collision

--- lone-wanderer-game/Content/Tilemaps/Terrain.tsx
+++ lone-wanderer-game/Content/Tilemaps/Terrain.tsx
@@ -1,4 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Terrain" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+<tileset version="1.10" tiledversion="1.10.2" name="mountain_landscape" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="mountain_landscape.png" width="512" height="512"/>
+ <tile id="0" probability="0.01"/>
+ <tile id="5" probability="0.01"/>
+ <tile id="78" probability="0.5"/>
+ <tile id="79" probability="0.5"/>
+ <tile id="94" probability="0.5"/>
+ <tile id="95" probability="0.5"/>
+ <tile id="151" probability="0.1"/>
+ <tile id="167" probability="0.1"/>
+ <tile id="168" probability="0.1"/>
 </tileset>
